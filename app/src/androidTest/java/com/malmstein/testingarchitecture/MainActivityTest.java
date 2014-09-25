d05723fdbeb408c8e313b0a286867ad4720cca1e
Iñaki <inaki@ideaknow.com>
2014-09-25 09:17:14 +0200

    spoon

--- app/src/androidTest/java/com/malmstein/testingarchitecture/MainActivityTest.java
+++ app/src/androidTest/java/com/malmstein/testingarchitecture/MainActivityTest.java
@@ -2,6 +2,8 @@ package com.malmstein.testingarchitecture;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.squareup.spoon.Spoon;
+
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
@@ -21,6 +23,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
     public void test_activity_shows_hello_world() {
         onView(withId(R.id.tv_hello_world)).check((matches(isDisplayed())));
+        Spoon.screenshot(getActivity(), "TAG");
     }
 
 }
